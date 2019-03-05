158fb21fcb215090410d79a3ff96c2fbd0e16b80
susmithagudapati <susmitha.gudapati@gmail.com>
2019-03-05 18:43:49 +0530

    change filename in app.py and create classifier.pkl

--- app/run.py
+++ app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/disaster_response.db')
+engine = create_engine('sqlite:///disaster_response.db')
 df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/model.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
