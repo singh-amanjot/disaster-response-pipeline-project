a77b16a2fb8c165b95b0d357fa4484e007d237d3
susmithagudapati <susmitha.gudapati@gmail.com>
2019-03-05 16:00:39 +0530

    tune parameters and train moel

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,85 @@
 import sys
 
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+import pickle
+import pandas as pd
+import numpy as np
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline
+
+from sqlalchemy import create_engine
+
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('messages', engine)
+    X = df['message'].values
+    Y = df.drop(['id','message','original','genre'], axis=1)
+    category_names = Y.columns
+    
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+  
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    
+    # build a pipeline
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    # params dict to tune a model
+    parameters = {
+        'clf__estimator__min_samples_split': [2, 4],
+        'clf__estimator__max_features': [None, 'log2', 'auto', 'sqrt'],
+        'clf__estimator__criterion': ['gini', 'entropy'],
+        'clf__estimator__max_depth': [None, 25, 50, 100, 150, 200],
+    }
+
+    # instantiate a gridsearchcv object with the params defined
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
+    
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    # predict on test data with tuned params
+    y_pred = model.predict(X_test)
+    
+    print(classification_report(Y_test.values, y_pred,
+     target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+    
+    # export model to pickle file
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
